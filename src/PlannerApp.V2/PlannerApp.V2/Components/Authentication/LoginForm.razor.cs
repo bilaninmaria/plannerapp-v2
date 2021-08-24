17952713504137844f85c4d6beb68cea85ce8c05
Maria <maria@livehosting.ro>
2021-08-24 12:43:35 +0300

    Create the AuthLayout and the LoginForm.

---
+++ src/PlannerApp.V2/PlannerApp.V2/Components/Authentication/LoginForm.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using PlannerApp.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PlannerApp.V2.Components
+{
+    public partial class LoginForm : ComponentBase
+    {
+        [Inject]
+        public HttpClient HttpClient { get; set; }
+
+        private LoginRequest _model = new();
+        private async Task LoginUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
