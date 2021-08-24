30a79c3b3b22095d51f0b38fe9209fe43dbf5e03
Maria <maria@livehosting.ro>
2021-08-24 20:25:09 +0300

    Create and validate the model of registering a user.

---
+++ src/PlannerApp.V2/PlannerApp.Shared/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using PlannerApp.Shared.Models;
+
+namespace PlannerApp.Shared.Validators
+{
+    public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+    {
+        public RegisterRequestValidator()
+        {
+            RuleFor(p => p.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .WithMessage("Email is required.");
+
+            RuleFor(p => p.FirstName)
+                .NotEmpty()
+                .WithMessage("First name is required.")
+                .MaximumLength(25)
+                .WithMessage("First name must be less then 25 characters.");
+
+            RuleFor(p => p.LastName)
+               .NotEmpty()
+               .WithMessage("Last name is required.")
+               .MaximumLength(25)
+               .WithMessage("Last name must be less then 25 characters.");
+
+            RuleFor(p => p.Password)
+               .NotEmpty()
+               .WithMessage("Password is required.")
+               .MinimumLength(5)
+               .WithMessage("Password must have at least 5 characters.");
+
+            RuleFor(p => p.ConfirmPassword)
+              .Equal(p => p.Password)
+              .WithMessage("Confirm password doesn't match the password.");
+        }
+    }
+}
